5a89bfc31f829106f3e1c2ab1ae3a5df17cdb19e
Christian <tellezc007@gmail.com>
2022-05-29 19:00:10 -0700

    add base files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="Header">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
