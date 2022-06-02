5c745b67bab351af8eb4fe8d634e2fb702594075
CT <tellezc007@gmail.com>
2022-06-01 19:40:29 -0700

    add styling to sections for project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-// import Navigation from './components/Navigation';
-// import Project from './components/Project';
-// import Footer from './components/Footer';
 import './App.css'
 
 
